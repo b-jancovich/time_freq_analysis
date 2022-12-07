d467db5827f53def97460956fa4103f3bf4af431
Ben Jancovich <116327520+b-jancovich@users.noreply.github.com>
2022-12-08 07:51:00 +1100

    Last update before branch
    - Changed some variable names for consistency
    - Removed old unused statistical tests and plotting
    - Found bug relating to image resizing to match ground truth. Fix requires a big change. Branching to fix...

--- groundtruth_comparison.m
+++ groundtruth_comparison.m
@@ -57,14 +57,14 @@ powerscaling = 'lin';       % Plot power as 'lin' (W) or 'log' (dBW)
 
 % STFT Window Sizes
 n_fft = 2*((fs/2)/f_res);   % spectrogram FFT length (samples)
-win1 = n_fft/5;             % Window length for longSTFT (samples)
-win2 = 50;                  % Window length for shortSTFT (samples)
-overlap1 = 75;              % Window overlap % for longSTFT
-overlap2 = 75;              % Window Overlap % for shortSTFT
+win1 = n_fft/5;             % Window length for long STFT (samples)
+win2 = 50;                  % Window length for short STFT (samples)
+overlap1 = 75;              % Window overlap % for long STFT
+overlap2 = 75;              % Window Overlap % for short STFT
 
 % CWT Parameters
-tbp = 200;      % Time bandwidth product of Morse wavelet. Scalar, >= 3, <= 120. original value 100
-gamma = 14;      % Symmetry of the wavelet. 3 = symmetric.
+tbp = 120;      % Time bandwidth product of Morse wavelet.
+gamma = 3;      % Symmetry of the wavelet. 3 = symmetric.
 vpo = 48;       % Voices per Octave. Must be in range 10 : 48
 
 % Superlet Parameters
@@ -168,7 +168,7 @@ all_MAT(end-(fmin/f_res):end, :) = 0;
 modMAT = repmat(mod,[n_freqs_total, 1]);
 all_MAT = all_MAT .* modMAT;
 
-% Add some gaussian smoothing to the ground truth matrix
+% Add some gaussian smoothing to the ground truth matrix'
 all_MAT = imgaussfilt(all_MAT, sigma);
 
 % Gaussian has changed range of values. Rescale to 0-1.
@@ -182,17 +182,19 @@ all_MAT = all_MAT.^2;
 
 %% Compute Transforms
 
-% Compute STFT
-[stft_longwin, stftlong_freq, stftlong_time] = spectrogram(signal, ...
-    win1, ceil(win1*(overlap1/100)), n_fft, fs, "yaxis");
+% Compute short windowed STFT
 [stft_shortwin, stftshort_freq, stftshort_time] = spectrogram(signal, ...
     win2, ceil(win2*(overlap2/100)), n_fft, fs, "yaxis");
 
-% Compute CWT
-[cwlet, f_cwt] = cwt(signal, fs, WaveletParameters = [gamma, tbp], FrequencyLimits=[fmin fmax]);
+% Compute long windowed STFT
+[stft_longwin, stftlong_freq, stftlong_time] = spectrogram(signal, ...
+    win1, ceil(win1*(overlap1/100)), n_fft, fs, "yaxis");
 
-% Compute superlets
-superlets = nfaslt(signal, fs, [dcfilt, fmax], n_freqs_total, c1, order, mult);
+% Compute CWT
+[cwlet, f_cwlet] = cwt(signal, fs, VoicesPerOctave=vpo, WaveletParameters = [gamma, tbp], FrequencyLimits=[fmin fmax]);
+
+% Compute SLT
+slt = nfaslt(signal, fs, [dcfilt, fmax], n_freqs_total, c1, order, mult);
 
 
 %% Unit Convertsions & Normalizations
@@ -202,7 +204,7 @@ superlets = nfaslt(signal, fs, [dcfilt, fmax], n_freqs_total, c1, order, mult);
 stft_longwin_plot = (abs(stft_longwin).^2);     % spectrogram() returns complex data.
 stft_shortwin_plot = (abs(stft_shortwin).^2);   % spectrogram() returns complex data.
 cwlet_plot = (abs(cwlet) .^2);                  % cwt() returns complex data.
-superlets_plot = (superlets .^2);               % nfaslt returns magnitude.
+slt_plot = (slt .^2);                           % nfaslt returns magnitude.
 
 % If powerscaling is set to plot power in dB, do log conversions:
 switch powerscaling
@@ -211,14 +213,14 @@ switch powerscaling
         stft_longwin_plot = 10*log10(stft_longwin_plot / 1);
         stft_shortwin_plot = 10*log10(stft_shortwin_plot / 1);
         cwlet_plot = 10*log10(cwlet_plot / 1);
-        superlets_plot = 10*log10(superlets_plot / 1);
+        slt_plot = 10*log10(slt_plot / 1);
 
         % Log conversions will result in some -inf values.
         % Replace -inf with the minimum value in each matrix.
         stft_longwin_plot(isinf(stft_longwin_plot)) = min(stft_longwin_plot(~isinf(stft_longwin_plot)), [], 'all');
         stft_shortwin_plot(isinf(stft_shortwin_plot)) = min(stft_shortwin_plot(~isinf(stft_shortwin_plot)), [], 'all');
         cwlet_plot(isinf(cwlet_plot)) = min(cwlet_plot(~isinf(cwlet_plot)), [], 'all');
-        superlets_plot(isinf(superlets_plot)) = min(superlets_plot(~isinf(superlets_plot)), [], 'all');
+        slt_plot(isinf(slt_plot)) = min(slt_plot(~isinf(slt_plot)), [], 'all');
     case 'lin'
 end
 
@@ -226,36 +228,37 @@ end
 stft_shortwin_plot = stft_shortwin_plot ./ max((stft_shortwin_plot), [], 'all');
 stft_longwin_plot = stft_longwin_plot ./ max((stft_longwin_plot), [], 'all');
 cwlet_plot = cwlet_plot ./ max((cwlet_plot), [], 'all');
-superlets_plot = superlets_plot ./ max((superlets_plot), [], 'all');
+slt_plot = slt_plot ./ max((slt_plot), [], 'all');
 
 % Data for use in measures of error
 % Convert algorithm outputs to power (W)
 stft_longwin = abs(stft_longwin).^2;    % spectrogram returns complex data.
 stft_shortwin = abs(stft_shortwin).^2;  % spectrogram returns complex data.
 cwlet = abs(cwlet) .^2;                 % cwt returns complex data.
-superlets = superlets .^2;              % nfaslt returns magnitude.
+slt = slt .^2;              % nfaslt returns magnitude.
 
 % Normalize to max = 1
 stft_shortwin = stft_shortwin ./ max((stft_shortwin), [], 'all');
 stft_longwin = stft_longwin ./ max((stft_longwin), [], 'all');
 cwlet = cwlet ./ max((cwlet), [], 'all');
-superlets = superlets ./ max((superlets), [], 'all');
+slt = slt ./ max((slt), [], 'all');
 
 
-%% Compute frequency resolutions
+%% Compute time & frequency resolutions
 
 % STFT Resolutions
 short_STFT_fres = (fmax-fmin) / win2;
 long_STFT_fres = (fmax-fmin) / win1;
+short_STFT_tres = (win2-(ceil(win2*(overlap2/100))))*(1/fs);
+long_STFT_tres = (win1-(ceil(win1*(overlap1/100))))*(1/fs);
 
 % CWT Resolutions
 idx1 = 1;
 idx2 = 2;
-cwtfrqs = flip(f_cwt);
-
-for i = 1:length(cwtfrqs)
-    cwt_fres_vec(i) = cwtfrqs(idx2) - cwtfrqs(idx1);
-    if idx2 < length(cwtfrqs)
+cwletfrqs = flip(f_cwlet);
+for i = 1:length(cwletfrqs)
+    cwlet_fres_vec(i) = cwletfrqs(idx2) - cwletfrqs(idx1);
+    if idx2 < length(cwletfrqs)
         idx1 = idx1 +1;
         idx2 = idx2 +1;
     end
@@ -263,11 +266,17 @@ end
 
 %% Compute Error
 
-% All matrices must be the same size as ground truth
-stft_shortwin_resz = imresize(stft_shortwin,[n_freqs_total, n_samps_total]);
-stft_longwin_resz = imresize(stft_longwin,[n_freqs_total, n_samps_total]);
-cwt_resz = imresize(cwlet,[n_freqs_total, n_samps_total]);
-superlets_resz = imresize(superlets,[n_freqs_total, n_samps_total]);
+% All matrices must be the same size as groundtruth.
+% Scale all to (n_freqs_total x n_samps_total).
+stft_shortwin_resz = imresize(stft_shortwin, [n_freqs_total, n_samps_total]);
+stft_longwin_resz = imresize(stft_longwin, [n_freqs_total, n_samps_total]);
+cwlet_resz= imresize(cwlet, [n_freqs_total, n_samps_total]);
+slt_resz = imresize(slt, [n_freqs_total, n_samps_total]);
+% The block above is incorrect. imresize() warps the image.
+% Have experimented with resize methods, but because cwt has inherently
+% non-linear frequency scaling, no linear resize method will work. Need to
+% generate a ground truth for each method. Branching in Git for this
+% change.
 
 % Compare similarity to ground truth via Root Mean Square Error:
 stft_shortwin_freqerror = mean(rmse(stft_shortwin_resz, all_MAT, 1));
@@ -278,81 +287,24 @@ stft_longwin_freqerror = mean(rmse(stft_longwin_resz, all_MAT, 2));
 stft_longwin_timeerror = mean(rmse(stft_longwin_resz, all_MAT, 1));
 stft_longwin_totalerror = rmse(stft_longwin_resz, all_MAT, 'all');
 
-cwt_freqerror = mean(rmse(cwt_resz, all_MAT, 2));
-cwt_timeerror = mean(rmse(cwt_resz, all_MAT, 1));
-cwt_totalerror = mean(rmse(cwt_resz, all_MAT, 'all'));
+cwlet_freqerror = mean(rmse(cwlet_resz, all_MAT, 2));
+cwlet_timeerror = mean(rmse(cwlet_resz, all_MAT, 1));
+cwlet_totalerror = mean(rmse(cwlet_resz, all_MAT, 'all'));
 
-superlet_freqerror = mean(rmse(superlets_resz, all_MAT, 2));
-superlet_timeerror = mean(rmse(superlets_resz, all_MAT, 1));
-superlet_totalerror = rmse(superlets_resz, all_MAT, 'all');
+slt_freqerror = mean(rmse(slt_resz, all_MAT, 2));
+slt_timeerror = mean(rmse(slt_resz, all_MAT, 1));
+slt_totalerror = rmse(slt_resz, all_MAT, 'all');
 
 % Compare similarity to ground truth via Structural Similarity Index:
 stft_shortwin_SSIM = ssim(stft_shortwin_resz, all_MAT);
 stft_longwin_SSIM = ssim(stft_longwin_resz, all_MAT);
-cwt_SSIM = ssim(cwt_resz, all_MAT);
-superlet_SSIM = ssim(superlets_resz, all_MAT);
-
-% Compare similarity to ground truth via Peak Signal To Noise Ratio:
-stft_shortwin_PSNR = psnr(stft_shortwin_resz, all_MAT);
-stft_longwin_PSNR = psnr(stft_longwin_resz, all_MAT);
-cwt_PSNR = psnr(cwt_resz, all_MAT);
-superlet_PSNR = psnr(superlets_resz, all_MAT);
-
-% Compare similarity to ground via Jaccard Similarity
-tol = 0.001;
-stft_shortwin_jacsim = jacsim(stft_shortwin_resz, all_MAT, tol);
-stft_longwin_jacsim = jacsim(stft_longwin_resz, all_MAT, tol);
-cwt_jacsim = jacsim(cwt_resz, all_MAT, tol);
-superlet_jacsim = jacsim(superlets_resz, all_MAT, tol);
-
-% Compare similarity to ground truth via IMMSE:
-stft_shortwin_immse = immse(stft_shortwin_resz, all_MAT);
-stft_longwin_immse = immse(stft_longwin_resz, all_MAT);
-cwt_immse = immse(cwt_resz, all_MAT);
-superlet_immse = immse(superlets_resz, all_MAT);
-
-% Dumb Error
-stft_shortwin_subtractsim = sum(abs(stft_shortwin_resz - all_MAT), 'all') ./ (n_freqs_total * n_samps_total);
-stft_longwin_subtractsim = sum(abs(stft_longwin_resz - all_MAT), 'all')./ (n_freqs_total * n_samps_total);
-cwt_subtractsim = sum(abs(cwt_resz - all_MAT), 'all')./ (n_freqs_total * n_samps_total);
-superlet_subtractsim = sum(abs(superlets_resz - all_MAT), 'all')./ (n_freqs_total * n_samps_total);
-
-% Binary Error
-threshold = 0.01;
-stft_shortwin_binerr = binaryerrdetect(stft_shortwin_resz, all_MAT, threshold);
-stft_longwin_binerr = binaryerrdetect(stft_longwin_resz, all_MAT, threshold);
-cwt_binerr = binaryerrdetect(cwt_resz, all_MAT, threshold);
-superlet_binerr = binaryerrdetect(superlets_resz, all_MAT, threshold);
-
-
-% Mutual Information (Mutual Information measures using kNN for both
-% continuous and categorical (discrete) variables) - https://github.com/otoolej/mutual_info_kNN
-% Compute across frequencies
-freqsmooth = 2; % Sets nearest neighbour (Hz)
-k = freqsmooth / f_res; % number of nearest neighbours
-for i = 1:n_freqs_total
-    stft_longwin_mi_freqsim(i) = mi_cont_cont(stft_longwin_resz(i,:), all_MAT(i,:), k);
-    stft_shortwin_mi_freqsim(i) = mi_cont_cont(stft_shortwin_resz(i,:), all_MAT(i,:), k);
-    cwt_mi_freqsim(i) = mi_cont_cont(cwt_resz(i,:), all_MAT(i,:), k);
-    superlet_mi_freqsim(i) = mi_cont_cont(superlets_resz(i,:), all_MAT(i,:), k);
-end
-% Compute across Times
-timesmooth = 20 ; % Sets nearest neigbour (ms)
-k = fs * 0.1; % number of nearest neighbours (samps)
-for i = 1:n_samps_total
-    stft_longwin_mi_timesim(i) = mi_cont_cont(stft_longwin_resz(:,i), all_MAT(:,i), k);
-    stft_shortwin_mi_timesim(i) = mi_cont_cont(stft_shortwin_resz(:,i), all_MAT(:,i), k);
-    cwt_mi_timesim(i) = mi_cont_cont(cwt_resz(:,i), all_MAT(:,i), k);
-    superlet_mi_timesim(i) = mi_cont_cont(superlets_resz(:,i), all_MAT(:,i), k);
-end
+cwlet_SSIM = ssim(cwlet_resz, all_MAT);
+slt_SSIM = ssim(slt_resz, all_MAT);
 
 % Dynamic STFT Names
 stftshort_name = ['STFT, ', num2str(win2), 'pt. Window']; %, num2str(overlap2), ' % Overlap'
 stftlong_name = ['STFT, ', num2str(win1), 'pt. Window']; % , num2str(overlap1), ' % Overlap'
-% Tabluate Results
-varnames = ['RMSE, Frequency Axis', 'RMSE, Time Axis', 'RMSE Total', ...
-    'MSE', 'SSI', 'PSNR'];
-rownames = [stftshort_name, stftlong_name, 'CWT', 'SWT'];
+
 
 
 %% Plot Figure 1 - Time Domain Signal
@@ -405,17 +357,12 @@ saveas(gcf,'Timedomain_Test_signal','svg')
 % Collate Error data for plotting
 xlabels1 = categorical({'Freq Axis', 'Time Axis', 'Total RMSE'});
 xlabels1 = reordercats(xlabels1,{'Freq Axis', 'Time Axis', 'Total RMSE'});
-ydata1 = [stft_shortwin_freqerror, stft_longwin_freqerror, cwt_freqerror, superlet_freqerror;
-    stft_shortwin_timeerror, stft_longwin_timeerror, cwt_timeerror, superlet_timeerror;
-    stft_shortwin_totalerror, stft_longwin_totalerror, cwt_totalerror, superlet_totalerror];
+ydata1 = [stft_shortwin_freqerror, stft_longwin_freqerror, cwlet_freqerror, slt_freqerror;
+    stft_shortwin_timeerror, stft_longwin_timeerror, cwlet_timeerror, slt_timeerror;
+    stft_shortwin_totalerror, stft_longwin_totalerror, cwlet_totalerror, slt_totalerror];
 xlabels2 = categorical({[num2str(win2), 'pt ', 'STFT'], [num2str(win1), 'pt ', 'STFT'], 'CWT', 'SWT'});
 xlabels2 = reordercats(xlabels2,{[num2str(win2), 'pt ', 'STFT'], [num2str(win1), 'pt ', 'STFT'], 'CWT', 'SWT'});
-ydata2 = [stft_shortwin_SSIM, stft_longwin_SSIM, cwt_SSIM, superlet_SSIM];
-ydata3 = [stft_shortwin_PSNR, stft_longwin_PSNR, cwt_PSNR, superlet_PSNR];
-ydata4 = [stft_shortwin_immse, stft_longwin_immse, cwt_immse, superlet_immse];
-ydata5 = [stft_shortwin_jacsim, stft_longwin_jacsim, cwt_jacsim, superlet_jacsim];
-ydata6 = [stft_shortwin_subtractsim, stft_longwin_subtractsim, cwt_subtractsim, superlet_subtractsim];
-ydata7 = [stft_shortwin_binerr, stft_longwin_binerr, cwt_binerr, superlet_binerr];
+ydata2 = [stft_shortwin_SSIM, stft_longwin_SSIM, cwlet_SSIM, slt_SSIM];
 
 % Plot label rounding
 np1 = 3;
@@ -458,12 +405,8 @@ set(gca, FontSize=12, FontName='Calibri')
 set(gcf, 'Position', [50 50 1000 300])
 saveas(gcf,'Final_methods_analytical_RMSE_TF','svg')
 
-% Plot Other Measures
-figure(3)
-t3 = tiledlayout(1,3);
-
 % Plot SSI
-nexttile
+figure(3)
 b2 = bar(xlabels2, ydata2);
 xtips1_2 = b2(1).XEndPoints;
 ytips1_2 = b2(1).YEndPoints;
@@ -482,130 +425,9 @@ grid on
 set(gca, FontSize=12, FontName='Calibri')
 xtickangle(90)
 
-% Jaccard Similaritty
-nexttile
-b5= bar(xlabels2, ydata5);
-xtips1_5 = b5(1).XEndPoints;
-ytips1_5 = b5(1).YEndPoints;
-labels1_5 = string(round(b5(1).YData, np5));
-text(xtips1_5,ytips1_5,labels1_5,'HorizontalAlignment','center',...
-    'VerticalAlignment','bottom')
-ylabel 'JSI re. Ground Truth'
-ttl = title('b');
-tt1.FontWeight = 'bold';
-tt1.fontsize = 12;
-ttl.Units = 'Normalize'; 
-ttl.Position(1) = 0; % use negative values (ie, -0.1) to move further left
-ttl.HorizontalAlignment = 'left';  
-grid on
-set(gca, FontSize=12, FontName='Calibri')
-ylim([0 (round(max(ydata5)*1.3, np2-2))])
-xtickangle(90)
+set(gcf, 'Position', [50 100 350 300])
+saveas(gcf,'Final_methods_ERROR_analytical_SSI','svg')
 
-% Plot PSNR
-nexttile
-b3 = bar(xlabels2, ydata3);
-xtips1_3 = b3(1).XEndPoints;
-ytips1_3 = b3(1).YEndPoints;
-labels1_3 = string(round(b3(1).YData, np3));
-text(xtips1_3,ytips1_3,labels1_3,'HorizontalAlignment','center',...
-    'VerticalAlignment','bottom')
-ylabel 'PSNR re. Ground Truth'
-ttl = title('c');
-tt1.FontWeight = 'bold';
-tt1.fontsize = 12;
-ttl.Units = 'Normalize'; 
-ttl.Position(1) = 0; % use negative values (ie, -0.1) to move further left
-ttl.HorizontalAlignment = 'left';  
-ylim([0 (round(max(ydata3)*1.3, np2-2))])
-grid on
-set(gca, FontSize=12, FontName='Calibri')
-xtickangle(90)
-
-% Plot MSE
-% nexttile
-% b4 = bar(xlabels2, ydata4);
-% xtips1_4 = b4(1).XEndPoints;
-% ytips1_4 = b4(1).YEndPoints;
-% labels1_4 = string(round(b4(1).YData, np4));
-% text(xtips1_4,ytips1_4,labels1_4,'HorizontalAlignment','center',...
-%     'VerticalAlignment','bottom')
-% ylabel 'MSE re. Ground Truth'
-% title('Mean Squared Error', FontWeight='bold', fontsize=12)
-% ylim([0 0.012])
-% grid on
-% xtickangle(90)
-
-% % Matrix sum of Deltas
-% nexttile
-% b6= bar(xlabels2, ydata6);
-% xtips1_6 = b6(1).XEndPoints;
-% ytips1_6 = b6(1).YEndPoints;
-% labels1_6 = string(round(b6(1).YData, np6));
-% text(xtips1_6,ytips1_6,labels1_6,'HorizontalAlignment','center',...
-%     'VerticalAlignment','bottom')
-% ylabel 'Sum of Deltas re. Ground Truth'
-% title('Sum of Deltas', FontWeight='bold', fontsize=12)
-% grid on
-% ylim([0 0.1])
-% xtickangle(90)
-
-% % Binary Mean Error
-% nexttile
-% b7= bar(xlabels2, ydata7);
-% xtips1_7 = b7(1).XEndPoints;
-% ytips1_7 = b7(1).YEndPoints;
-% labels1_7 = string(round(b7(1).YData, np7));
-% text(xtips1_7,ytips1_7,labels1_7,'HorizontalAlignment','center',...
-%     'VerticalAlignment','bottom')
-% ylabel 'BME re. Ground Truth'
-% title(['Binary Mean Error, T= ', num2str(threshold)], FontWeight='bold', fontsize=12)
-% grid on
-% ylim([0 0.6])
-% xtickangle(90)
-
-t3.TileSpacing = 'compact';
-t3.Padding = 'loose';
-set(gcf, 'Position', [50 100 1000 300])
-saveas(gcf,'Final_methods_ERROR_analytical_SSI_PSNR_JACSIM','svg')
-% 
-% % Mutual Information - Frequency
-% figure(11)
-% t4 = tiledlayout(1,2);
-% nexttile
-% p(1) = plot(f_vec_total, stft_shortwin_mi_freqsim, 'red', LineWidth=1);
-% hold on
-% p(2) = plot(f_vec_total, stft_longwin_mi_freqsim, 'blue', LineWidth=1);
-% p(3) = plot(f_vec_total, cwt_mi_freqsim, 'green', LineWidth=1);
-% p(4) = plot(f_vec_total, superlet_mi_freqsim, 'magenta', LineWidth=1);
-% lgd1 = legend(stftshort_name, stftlong_name, 'CWT', 'SWT', Location='northwest');
-% grid on
-% xlabel('Frequency (Hz)')
-% ylabel('Mutual Information Estimate')
-% title('Mutual Information, Frequency Axis re. Ground Truth', FontWeight='bold', fontsize=12, FontName='Calibri')
-% xlim([fmin fmax])
-% ylim([0 7])
-% set(gca, FontSize=12, FontName='Calibri')
-% 
-% 
-% % Mutual Information - Time
-% nexttile
-% pp(1) = plot(t_vec_total, stft_longwin_mi_timesim, 'red -.', LineWidth=1);
-% hold on
-% pp(2) = plot(t_vec_total, stft_shortwin_mi_timesim, 'blue --' , LineWidth=1);
-% pp(3) = plot(t_vec_total, cwt_mi_timesim, 'green', LineWidth=1);
-% pp(4) = plot(t_vec_total, superlet_mi_timesim, 'magenta :', LineWidth=2);
-% lgd2 = legend(stftshort_name, stftlong_name, 'CWT', 'SWT', Location='northwest');
-% grid on
-% xlim([0 7])
-% ylim([0 12])
-% xlabel('time (s)')
-% set(gca, FontSize=12, FontName='Calibri')
-% ylabel('Mutual Information Estimate')
-% title('Mutual Information, Time Axis re. Ground Truth', FontWeight='bold', fontsize=12, FontName='Calibri')
-% 
-% set(gcf, 'Position', [50 100 1000 350])
-% saveas(gcf,'Final_methods_MutualInfo_TF','svg')
 
 %% Plot More Figures - Time-Freq Representations
 
@@ -762,7 +584,7 @@ ax.MinorGridAlpha = 0.15;
 
 % Plot CWT
 nexttile
-surf(f_cwt, t_vec_total, cwlet_plot', EdgeColor="none", FaceColor="texturemap")
+surf(f_cwlet, t_vec_total, cwlet_plot', EdgeColor="none", FaceColor="texturemap")
 ttl = title('b');
 tt1.FontWeight = 'bold';
 tt1.fontsize = 12;
@@ -822,7 +644,7 @@ ax.MinorGridAlpha = 0.15;
 
 % Plot Superlets
 nexttile
-surf(f_vec_total, t_vec_total, superlets_plot', EdgeColor="none", FaceColor="texturemap")
+surf(f_vec_total, t_vec_total, slt_plot', EdgeColor="none", FaceColor="texturemap")
 ttl = title('b');
 tt1.FontWeight = 'bold';
 tt1.fontsize = 12;
