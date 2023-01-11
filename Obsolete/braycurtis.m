0c98796b3e26a90dfb9627758bf0fca9a2177a6d
Ben Jancovich <116327520+b-jancovich@users.noreply.github.com>
2023-01-11 13:16:12 +1100

    Cleanup
    Removed irrelevant files
    Added subdirectory containing SLT demo tool installer, readme, dummy test signal & licence.

--- Obsolete/braycurtis.m
+++
@@ -1,9 +0,0 @@
-function S = braycurtis(A, B)
-
-D = abs(A - B) ./ (A + B);
-D(A == 0 | B == 0) = nan;
-S = 1 - D / max(D(:));
-S(isnan(S)) = 0;
-S = mean(S, 'all');
-
-end
--- Obsolete/plot_profiling.m
+++
@@ -1,6 +0,0 @@
-figure (10)
-tiledlayout('flow')
-nexttile
-F = specplot(swlet, t_vec, f_vec, 'superlet', freqlim, timelim);
-nexttile
-F = specplot(cwlet, t_vec, f_vec, 'contwavlet', freqlim, timelim);
